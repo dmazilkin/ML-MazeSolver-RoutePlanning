2ba1bae786132d3b12d1101d3002bb3aec3dacc9
Dmitrii Mazilkin <dim.mazilkin@gmail.com>
2024-11-17 12:51:09 +0100

    Implement class Maze.

---
+++ DataSctructures/base_maze.py
@@ -0,0 +1,44 @@
+class Maze:
+    def __init__(self, file_path):
+        self._maze = self._read_maze_from_file(file_path)
+        self._maze_height = len(self._maze)
+        self._maze_width = len(self._maze[0])
+        self._start, self._stop = self._find_start_and_stop_coords()
+
+    def get_processed_maze(self):
+        return self._maze
+
+    def get_maze_shape(self):
+        return self._maze_height, self._maze_width
+
+    def get_start(self):
+        return self._start
+
+    def get_stop(self):
+        return self._stop
+
+    def _read_maze_from_file(self, file_path):
+        content = []
+        with open(file_path, 'r') as file:
+            for line in file:
+                line = [self._process_symbol(symbol) for symbol in list(line.rstrip())]
+                content.append(line)
+        return content
+
+    def _process_symbol(self, symbol):
+        if symbol == '#':
+            return 0
+        elif symbol == ' ':
+            return 1
+        else:
+            return symbol
+
+    def _find_start_and_stop_coords(self):
+        start, stop = None, None
+        for row in range(self._maze_height):
+            for column in range(self._maze_width):
+                if self._maze[row][column] == 'A':
+                    start = (row, column)
+                if self._maze[row][column] == 'B':
+                    stop = (row, column)
+        return start, stop
